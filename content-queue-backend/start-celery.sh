1a8006fcc42b588fb93f68b11a6236491e11e23c
enpuyou <youenpu@gmail.com>
2026-01-15 00:07:41 -0800

    Force clean virtualenv rebuild to remove cached psycopg2

--- content-queue-backend/start-celery.sh
+++ content-queue-backend/start-celery.sh
@@ -3,6 +3,9 @@
 
 set -e  # Exit on error
 
+echo "=== Removing old virtualenv ==="
+rm -rf .venv
+
 echo "=== Installing main dependencies from poetry.lock ==="
 poetry install --no-root --only main
 
